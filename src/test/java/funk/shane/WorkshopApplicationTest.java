0747e8d357ff823d5ff115caec1738c92504b0ea
Shane Funk <shane@springventuregroup.com>
2017-10-20 10:44:03 -0500

    setting up project

---
+++ src/test/java/funk/shane/WorkshopApplicationTest.java
@@ -0,0 +1,18 @@
+package funk.shane;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * ©2017 Spring Venture Group - fun-school-workshop
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
