9cc8d6705f9b2fd70ff08cdbc6b9644d8b0069d8
Shane Funk <shane@springventuregroup.com>
2017-10-31 12:21:41 -0500

    Sample and learning code

---
+++ src/main/java/funk/shane/workshop/controller/WorkshopController.java
@@ -0,0 +1,10 @@
+package funk.shane.workshop.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * © Fun-School-Workshop
+ */
+@Controller
+public class WorkshopController {
+}
